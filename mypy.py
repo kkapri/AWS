d18eccb7940d6d88dfac21b984035811ec127ed1
kkapri <kapri.kapil007@gmail.com>
2020-10-08 15:53:11 +0530

    mypy.py added

---
+++ mypy.py
@@ -0,0 +1,15 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> int x
+SyntaxError: invalid syntax
+>>> int x:
+	
+SyntaxError: invalid syntax
+>>> x=5
+>>> y=10
+>>> z=x+y
+>>> print(z)
+15
+>>> print('sum of ',x,' and ',y,' is : ',z)
+sum of  5  and  10  is :  15
+>>> 
